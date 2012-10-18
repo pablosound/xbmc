dacaefa708407616ba721b8283541693d71f42b2
Pablo Aguilar <pablosound@gmail.com>
2012-10-18 05:03:16 -0300

    XBMClisto

---
+++ XBMC-TRY/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  XBMC-TRY
+//
+//  Created by Pablo Aguilar on 11/10/12.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ XBMC-TRY/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  XBMC-TRY
+//
+//  Created by Pablo Aguilar on 11/10/12.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
